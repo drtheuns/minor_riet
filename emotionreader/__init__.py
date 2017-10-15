abb0d3be20cbe4968e2bf0ff5ce96ecf3cc2dc0a
Randall Theuns <randall.theuns@gmail.com>
2017-10-15 21:55:08 +0200

    Rename src folder to emotionreader

---
+++ emotionreader/__init__.py
@@ -0,0 +1,8 @@
+"""An open-source emotion detection system built for the minor
+Research in Emerging Technologies at the University of Applied Sciences.
+"""
+
+__author__ = "Randall Theuns"
+__version_info__ = (0, 1)
+__version__ = '.'.join((str(e) for e in __version_info__))
+__description__ = __doc__
